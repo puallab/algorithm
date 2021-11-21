718a19803f2e9c84f1648c62f651706e4cea8df3
puallab <pdh5056@gmail.com>
2021-11-22 02:54:27 +0900

    201122

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -2,7 +2,8 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int n;
+    static int n, k, x_min = (int)1e6, x_max = 0;
+    static int[] arr = new int[1000001];
     public static void main(String[] args) throws Exception {
         input();
         pro();
@@ -10,22 +11,36 @@ public class Main {
     
     public static void input() throws Exception{
        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-       n = Integer.parseInt(br.readLine());
+       StringTokenizer st = new StringTokenizer(br.readLine());
+       n = Integer.parseInt(st.nextToken());
+       k = Integer.parseInt(st.nextToken());  
+       for(int i =0; i<n; i++){
+           st = new StringTokenizer(br.readLine());
+           int g = Integer.parseInt(st.nextToken());
+           int x = Integer.parseInt(st.nextToken());
+           arr[x] = g;
+           x_min = Math.min(x_min, x);
+           x_max = Math.max(x_max, x);
+       }
     }
 
     public static void pro(){
-        int rememberWeight = 1, currentWeight =2;
-        StringBuilder sb = new StringBuilder();
-        
-        while (rememberWeight< currentWeight && currentWeight < n){
-            int value = currentWeight*currentWeight - rememberWeight*rememberWeight;
-            if(value == n) sb.append(currentWeight++).append(' ');
-            else if( value < n) currentWeight++;
-            else rememberWeight++;
+        long ans =0, cnt = 0;
+        k = 2*k+1;
+        int m = 0, right = x_min;
+        for(int left = x_min; left <= x_max; left++){
+            while(m < k && right < x_max+1){
+                cnt += arr[right];
+                m++;
+                right++;
+            }
+            if(m == k){
+                ans = Math.max(cnt, ans);
+            }
+            cnt -= arr[left];
+            m--;
         }
-        
-        if(sb.toString().equals("")) System.out.println("-1");
-        else System.out.println(sb);
+        System.out.println(ans);
     }
 
 }
