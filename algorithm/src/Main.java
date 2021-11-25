7778963a0634f5683ca2a7a9141921ed93ca3cc0
puallab <pdh5056@gmail.com>
2021-11-25 20:37:59 +0900

    Dp : 1학년

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -1,78 +1,46 @@
 import java.util.*;
 import java.io.*;
 
-
-
 public class Main {
-    static int[][] board;
-    static int n, k, l;
-    static int[][] pair;
+ 
     public static void main(String[] args) throws Exception {
-        input();
-        pro();
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n+1];
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+
+        long[][] dp = new long[n+1][21];
+
+        dp[0][arr[0]] = 1;
+
+        for(int i = 1; i<= n-2; i++){
+            int k = arr[i];
+            for(int j= 0; j<21; j++){
+                if(dp[i-1][j] > 0){
+                    if( j - k >=  0) dp[i][j - k] += dp[i-1][j];
+                    if( j + k <= 20) dp[i][j+k] += dp[i-1][j];
+                }
+            }
+        }
+
+        System.out.println(dp[n-2][arr[n-1]]);
+
+
     }
     
     public static void input() throws Exception{
-        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        k = Integer.parseInt(br.readLine());
-        board = new int[n][n];
-        for(int i =0; i<k; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int y = Integer.parseInt(st.nextToken())-1;
-            int x = Integer.parseInt(st.nextToken())-1;
-            board[y][x] = 1;
-        }
-
-        l = Integer.parseInt(br.readLine());
-        pair = new int[l][2];
-        for(int i =0; i<l; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int t = Integer.parseInt(st.nextToken());
-            int d = 0;
-            if(st.nextToken().equals("D")) d =1;
-            pair[i][0] = t; pair[i][1] = d;
-        }
+        
 
     }
 
     public static void pro(){
-        int ans = 0;
-        int idx = 0; //l에 관한 index 정보.
-        int dir = 1; //현재 방향을 나타냄
-
-        int[] dy = {0,1};
-        int[] dx = {1,0};
         
-        int[] head = {0, 0}; //현재 머리의 위치 기록.
-        boolean[][] frint = new boolean[n][n];
-        frint[0][0] = true;
-        Queue<Integer> qy = new LinkedList<>();
-        Queue<Integer> qx = new LinkedList<>();
-        qy.add(head[0]); qx.add(head[1]);
-        while(true){
-            int cy = head[0] + dy[dir];
-            int cx = head[1] + dx[dir];
-            if(cy < 0 || cy >= n || cx <0 || cx>=n) break;
-            if(frint[cy][cx] == true ) break;
-            frint[cy][cx] = true;
-            if(board[cy][cx] == 1){
-                //사과있으면 그냥 머리만 늘리기.
-                board[cy][cx] = 0;
-                frint[cy][cx] =true;
-                qy.add(cy); qx.add(cx);
-            }else{
-                //사과 없으면 발자취 없에고 
-                //추가하기.
-                frint[cy][cx] = true;
-                int yy = qy.poll();
-                int xx = qx.poll();
-                frint[yy][xx] = false;
-            }   
-            ans++;
-        }
-
-        System.out.println(ans);
     }
 
    
