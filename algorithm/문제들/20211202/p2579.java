761968344e584476475a45a3c6e907db52fe009f
puallab <pdh5056@gmail.com>
2021-12-05 00:45:46 +0900

    dp : 계단 오르기

--- algorithm/문제들/20211202/p2579.java
+++ algorithm/문제들/20211202/p2579.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class p2579 {
 
     public static void main(String[] args) throws Exception {
        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
