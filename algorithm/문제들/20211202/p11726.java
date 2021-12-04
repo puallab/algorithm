5b902fd4a5b3f1ce8a5686dbf25db9ebdde258d2
puallab <pdh5056@gmail.com>
2021-12-05 00:45:30 +0900

    dp : 2xn 타일링

--- algorithm/문제들/20211202/p11726.java
+++ algorithm/문제들/20211202/p11726.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class p11726 {
 
     public static void main(String[] args) throws Exception {
        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
