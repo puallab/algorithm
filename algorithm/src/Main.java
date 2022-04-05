3b293a9a495d1352122346576f94efa7b25cb6f5
puallab <pdh5056@gmail.com>
2022-04-06 04:06:11 +0900

    [feat] .gitignore 설정

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -1,74 +1,21 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-
-public class Main {
-    static int t, f;
-    static Map<String, Integer> map;
-    static int[] root, cnt;
-    static class Node{
-        String n;
-
-    }
+public class Main{
     
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st=  new StringTokenizer(br.readLine());
-        t = Integer.parseInt(st.nextToken());
-        
-        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BitSet bit = new BitSet(1<<25);
         StringBuilder sb = new StringBuilder();
-        while(t -- > 0){
-            f = Integer.parseInt(br.readLine());
-            map = new HashMap<>();
-            root = new int[2*f];
-            cnt = new int[2*f];
-            Arrays.fill(cnt, 1);
-            int index = 0;
-            for(int i =0; i<f; i++){
-                st = new StringTokenizer(br.readLine());
-                String a = st.nextToken();
-                String b= st.nextToken();
-                if(!map.containsKey(a)){
-                    map.put(a, index);
-                    root[index] = index;
-                    index++;
-                }
-
-                if(!map.containsKey(b)){
-                    map.put(b, index);
-                    root[index] = index;
-                    index++;
-                }
-
-                sb.append(union(map.get(a), map.get(b)) + "\n");
-                
+        while(st.hasMoreTokens()){
+            int n = Integer.parseInt(st.nextToken());
+            if(!bit.get(n)){
+                bit.set(n);
+                sb.append(n + " ");
             }
         }
 
         System.out.println(sb);
-        
     }
-
-    static int find(int n){
-       if(root[n] == n) return n;
-       return root[n] = find(root[n]);
-    }
-
-    static int union(int a, int b){
-        a = find(a);
-        b = find(b);
-        if(a == b) return cnt[a];
-        if(a < b) {
-            root[b] = a;
-            return cnt[a] += cnt[b];
-        }
-        else{
-            root[a] = b;
-            return cnt[b] += cnt[a];
-        }
-    }
-       
 }
-
-
