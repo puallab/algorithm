30ba3b190e2b3cef26566f34ad5482d85f7eccc1
pdh5056 <pdh5056@gmail.com>
2022-04-28 11:52:05 +0900

    bfs; 통나무옮기기 (미완)

--- algorithm/문제들/20220427/boj1938.java
+++ algorithm/문제들/20220427/boj1938.java
@@ -6,13 +6,15 @@ public class boj1938 {
     static class State {
         int[] y = new int[3], x = new int[3];
         int cnt;
+        boolean shape; //  true: ㅡ, false: ㅣ
 
-        public State(int[] yy, int[] xx) {
+        public State(int[] yy, int[] xx, boolean flag) {
             for (int i = 0; i < 3; i++) {
                 y[i] = yy[i];
                 x[i] = xx[i];
             }
             cnt = 0;
+            shape = flag;
         }
 
         @Override
@@ -20,9 +22,7 @@ public class boj1938 {
 
             if(obj instanceof State){ 
                 State B = (State) obj; 
-                for(int i=0; i<3; i++){
-                    if(this.y[i] != B.y[i] || this.x[i] != B.x[i]) return false;
-                }
+                if(this.y[0] == B.y[0] && this.x[0] == B.x[0] && )
                 return true;
             }
 
@@ -75,9 +75,9 @@ public class boj1938 {
 
                     // 가로 확인
                     if (j < n - 2 && board[i].charAt(j + 1) == target) {
-                        return new State(new int[] { i, i, i }, new int[] { j, j + 1, j + 2 });
+                        return new State(new int[] { i, i, i }, new int[] { j, j + 1, j + 2 }, true);
                     } else if (i < n - 2 && board[i+1].charAt(j) == target) {
-                        return new State(new int[] { i, i + 1, i + 2 }, new int[] { j, j, j });
+                        return new State(new int[] { i, i + 1, i + 2 }, new int[] { j, j, j }, false);
                     }
 
                 }
@@ -122,7 +122,7 @@ public class boj1938 {
                 if(!isRange(cy[i],cx[i])) return null;
             }
 
-            cState = new State(cy, cx);
+            cState = new State(cy, cx, now.shape);
 
         }else{
             for(int i =0; i<8; i++){
@@ -130,8 +130,15 @@ public class boj1938 {
                 int cx = dx[i] + now.x[1];
                 if(!isRange(cy, cx)) return null;
             }
-
-            cState = new State(new int[]{now.y[0]+1, now.y[1], now.y[2]-1}, new int[]{now.x[0]+1, now.x[1], now.x[2]-1});
+            
+            // true : -, false :l;
+            if(now.shape){
+                if(now.x[0] < now.x[1]) cState = new State(new int[]{now.y[0]-1, now.y[1], now.y[2]+1}, new int[]{now.x[0]+1, now.x[1], now.x[2]-1}, false); 
+                else cState = new State(new int[]{now.y[0]+1, now.y[1], now.y[2]-1}, new int[]{now.x[0]-1, now.x[1], now.x[2]+1}, false); 
+            }else{
+                if(now.y[0] < now.y[1]) cState = new State(new int[]{now.y[0]+1, now.y[1], now.y[2]-1}, new int[]{now.x[0]+1, now.x[1], now.x[2]-1}, true);
+                else cState = new State(new int[]{now.y[0]-1, now.y[1], now.y[2]+1}, new int[]{now.x[0]+1, now.x[1], now.x[2]-1}, true);
+            }
             
         }
 
