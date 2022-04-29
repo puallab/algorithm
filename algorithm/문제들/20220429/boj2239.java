c3341115d2df50dc7dd6e9851192083781e567f2
pdh5056 <pdh5056@gmail.com>
2022-04-29 15:06:34 +0900

    백트래킹; 스도쿠

---
+++ algorithm/문제들/20220429/boj2239.java
@@ -0,0 +1,14 @@
+public class boj2239 {
+    public static void main(String[] args) throws Exception {
+        input();
+        pro();
+    }
+
+    static void input() throws Exception{
+
+    }
+
+    static void pro(){
+        
+    }
+}
