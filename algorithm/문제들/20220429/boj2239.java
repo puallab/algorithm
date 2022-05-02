0ff037162aac60da683384e61374a0c6d85f66cd
pdh5056 <pdh5056@gmail.com>
2022-05-02 20:58:07 +0900

    backTracking; 스도쿠

--- algorithm/문제들/20220429/boj2239.java
+++ algorithm/문제들/20220429/boj2239.java
@@ -5,11 +5,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class boj2239 {
-    static char[][] board = new char[9][9];
+    static int[][] board = new int[9][9];
     static boolean[][] colVis = new boolean[9][10];
     static boolean[][] rowVis = new boolean[9][10];
     static boolean[][] sqrVis = new boolean[9][10];
     static List<Integer> list = new ArrayList<>();
+    static boolean flag = false;
     
     public static void main(String[] args) throws Exception {
         input();
@@ -23,20 +24,54 @@ public class boj2239 {
             String t=  br.readLine();
 
             for(int j =0; j<9; j++){
-                board[i][j] = t.charAt(j);
-                if(board[i][j] != '0'){
-                    int val = board[i][j]-'0';
-                    colVis[j][val] = true;
-                    rowVis[i][val] = true;
-                    sqrVis[(i/3)*3 + j/3][val]  = true;
-                }
-                else list.add( i*9 + j );
+                
+                board[i][j] = t.charAt(j) -'0';
+                
+                colVis[j][board[i][j]] = true;
+                rowVis[i][board[i][j]] = true;
+                sqrVis[(i/3)*3 + j/3][board[i][j]]  = true;
+                
             }
         }
     }
 
     static void pro(){
         backTracking(0);
+    }
+
+    static void  backTracking(int idx){
+        
+        if(flag) return;
+        if(idx == 81 ){
+            flag = true;
+
+            showAns();
+            return;
+        }
+
+        int i = idx/9;
+        int j = idx%9;
+        if(board[i][j] != 0) backTracking(idx+1);
+        else{
+            for(int n=1; n <= 9; n++){
+
+                int k = (i/3)*3 + j/3;
+                if(colVis[j][n] || rowVis[i][n] ||  sqrVis[k][n]) continue;
+    
+                board[i][j] = n;
+                colVis[j][n] = rowVis[i][n] = sqrVis[k][n] = true;
+                backTracking(idx+1);
+                
+                colVis[j][n] = rowVis[i][n] = sqrVis[k][n] = false;
+                board[i][j] = 0;
+            }
+        }
+    }
+
+    static void showAns(){
+
+        System.out.println();
+        System.out.println();
 
         for(int i =0; i<9; i++){
             for(int j =0; j<9; j++){
@@ -45,27 +80,4 @@ public class boj2239 {
             System.out.println();
         }
     }
-
-    static boolean  backTracking(int idx){
-        if(idx == list.size()){
-            return true;
-        }
-
-        int i = list.get(idx)/9, j = list.get(idx)%9;
-        boolean flag = false;
-
-        for(int n=1; n <= 9; n++){
-
-            if(colVis[j][n] || rowVis[i][n] ||  sqrVis[(i/3)*3 + j/3][n]) continue;
-
-            board[i][j] = n + '0';
-            
-            flag = backTracking(idx+1);
-            if(flag) break;
-        }
-        
-        return flag;
-    }
-
-    
 }
