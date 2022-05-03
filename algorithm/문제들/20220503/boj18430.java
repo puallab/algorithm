9ca847fd6023b3f1baa42069e83369cd2f5ea848
pdh5056 <pdh5056@gmail.com>
2022-05-03 22:14:10 +0900

    백트래킹; 무기공학

--- algorithm/문제들/20220503/boj18430.java
+++ algorithm/문제들/20220503/boj18430.java
@@ -1,7 +1,96 @@
+import java.io.*;
+import java.util.*;
+
 public class boj18430 {
-    
-    
-    public static void main(String[] args) {
-        
+
+    static int n, m;
+    static int map[][];
+    static boolean vis[][];
+    static int ans = 0;
+    static int[][][] shape = { { { 0, 1 }, { -1, 0 } }, // 우 상
+            { { 0, 1 }, { 1, 0 } }, // 우 하
+            { { 0, -1 }, { -1, 0 } }, // 좌 상
+            { { 0, -1 }, { 1, 0 } } }; // 좌 하
+
+    public static void main(String[] args) throws Exception {
+        input();
+        pro();
+    }
+
+    static void input() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        map = new int[n][m];
+        vis = new boolean[n][m];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+    }
+
+    static void pro() {
+        if (n < 2 || m < 2 || n * m < 4)
+            ans = 0;
+        else
+            dfs(0, 0);
+
+        System.out.println(ans);
+    }
+
+    static void dfs(int idx, int sum) {
+
+        if (idx == m * n) {
+            ans = Math.max(sum, ans);
+            //show(sum);
+            return;
+        }
+
+        int i = idx / m;
+        int j = idx % m;
+        if (vis[i][j])
+            dfs(idx + 1, sum);
+        else {
+            for (int k = 0; k < 4; k++) {
+
+                int ry = shape[k][0][0] + i;
+                int rx = shape[k][0][1] + j;
+
+                int ly = shape[k][1][0] + i;
+                int lx = shape[k][1][1] + j;
+
+                if (!isValid(ry, rx) || !isValid(ly, lx))
+                    continue;
+                if (vis[ry][rx] || vis[ly][lx])
+                    continue;
+
+                vis[i][j] = vis[ry][rx] = vis[ly][lx] = true;
+                dfs(idx + 1, sum + map[ry][rx] + map[ly][lx] + map[i][j]*2);
+                vis[i][j] = vis[ry][rx] = vis[ly][lx] = false;
+            }
+            dfs(idx + 1, sum);
+        }
+
+    }
+
+    static boolean isValid(int y, int x) {
+        return (y >= 0 && x >= 0 && y < n && x < m);
+    }
+
+    static void show(int sum){
+        System.out.println();
+
+        for(int i =0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(vis[i][j]) System.out.print("1 ");
+                else System.out.print("0 ");
+            }
+            System.out.println();
+        }
+        System.out.println(sum);
     }
 }
