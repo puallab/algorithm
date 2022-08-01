63653c0c9e4007d5f71332e7c2b743dbfbfc2b7c
puallab <pdh5056@gmail.com>
2022-08-02 02:03:05 +0900

    수정

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -4,6 +4,9 @@ import java.util.*;
 
 public class Main{
     static int[][] board = new int[3][3];
+    static List<Integer> list = new ArrayList<>();
+    static int player, cnt;
+    static int ans = -1;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st; 
@@ -11,15 +14,93 @@ public class Main{
             st = new StringTokenizer(br.readLine());  
             for(int j =0; j<3; j++){
                 board[i][j] = Integer.parseInt(st.nextToken());
+                if(board[i][j] == 0 ){
+                    list.add(3*i+j);
+                }
             }
         }
-              
 
-        
+        if(list.size() % 2 != 0){
+            player = 1;
+        }else{
+            player = 2;
+        }
+        cnt = list.size();
+        dfs(player, list.get(list.size()-1));
+
+        if(ans == 1){
+            System.out.println("W");
+        }else if( ans == -1){
+            System.out.println("L");
+        }else{
+            System.out.println("D");
+        }
         
     }
 
-    
+    static int dfs(int turn, int idx){
+        
+        if(ans == 1){
+            return 1;
+        }
 
+        if(isGameOver(idx)){
+            if(turn != player){
+                return 1;
+            }else{
+                return -1;
+            }
+        }
+
+        if(cnt == 0){
+            ans = 0;
+            return 0;
+        }
+
+        int max = -2;
+
+        for(int i : list){
+            int y = i/3;
+            int x = i%3;
+            if(board[y][x] == 0){
+                board[y][x] = turn;
+                cnt -= 1;
+                max = Integer.min(max ,dfs(3-turn, i));
+                cnt += 1;
+                board[y][x] = 0;
+            }
+        }
+        return max;
+    }
+
+    static boolean isGameOver(int idx){
+        int y = idx/3;
+        int x = idx%3;
+        int k = board[y][x];
+
+        if(k == 0) return false;
+        //가로, 세로
+        if(board[y][0] == board[y][1] && board[y][0] == board[y][2]) return true;
+        if(board[0][x] == board[1][x] && board[0][x] == board[2][x]) return true;
+
+        // 좌상 대각선
+        if(y+x ==2 && board[2][0] == board[1][1] && board[0][2] == board[1][1]) return true;
+
+        // 우상 대각선
+        if(y == x && board[0][0] == board[1][1] && board[2][2] == board[1][1]) return true;
+        
+        return false;
+    }
+
+    static void showBoard(){
+        System.out.println("\n");
+        for(int i =0; i<3; i++){
+            for(int j =0; j<3; j++){
+                System.out.print( board[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("\n");
+    }
 }
 
