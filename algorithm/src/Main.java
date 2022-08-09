72c609470d3d4378ad8fd74ed9dcac6155e23ede
pdh5056 <pdh5056@gmail.com>
2022-08-09 17:26:57 +0900

    미완성

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -1,60 +1,45 @@
 import java.io.*;
+import java.lang.reflect.Array;
 import java.util.*;
+import java.util.concurrent.ArrayBlockingQueue;
 
 public class Main{
-    static int n, m, v;
-    static boolean[][] board;
-    static boolean[] vis;
-    static StringBuilder sb = new StringBuilder();
+    static int n;
+    static int[] parents;
+    static ArrayList<Integer>[] board;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        v = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(br.readLine());
+
+        board = new ArrayList[n+1];
+        for(int i=0; i<n+1; i++){
+            board[i] = new ArrayList<>();
+        }
+        parents = new int[n+1];
         
-        board = new boolean[n+1][n+1];
-        vis = new boolean[n+1];
-        for(int i =0; i<m; i++){
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            board[a][b] = board[b][a] = true;
+        for(int i=0; i<n-1; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a= Integer.parseInt(st.nextToken());
+            int b= Integer.parseInt(st.nextToken());
+            board[a].add(b);
+            board[b].add(a);
         }
 
-        dfs(v);
-        sb.append("\n");
-        bfs();
-        System.out.println(sb.toString());
+        parents[1] = -1;
+        dfs(1);
+
+        for(int i =2; i<n+1; i++){
+            System.out.println(parents[i]);
+        }
     }
+
 
     static void dfs(int from){
-        vis[from] = true;
-        sb.append(from + " ");
 
-        for(int i =1; i<n+1; i++){
-            if(board[from][i] && !vis[i]){
-                dfs(i);
-            }
-        }
+        
     }
 
-    static void bfs(){
-        vis = new boolean[n+1];
-        Queue<Integer> q = new LinkedList<>();
-        q.add(v);
-        vis[v] = true;
-        while(!q.isEmpty()){
-            int now = q.poll();
-            sb.append(now + " ");
-            for(int i =1; i<n+1; i++){
-                if(board[now][i] && !vis[i]){
-                    vis[i] = true;
-                    q.add(i);
-                }
-            }
-        }
-    }
+  
 
     
    
