be437fd07c6ba8d5845ee7a2b46466fb2ba9a56c
pdh5056 <pdh5056@gmail.com>
2022-08-09 15:19:56 +0900

    문제정리

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -2,46 +2,61 @@ import java.io.*;
 import java.util.*;
 
 public class Main{
-    static int n, t, ans;
+    static int n, m, v;
     static boolean[][] board;
     static boolean[] vis;
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        n = Integer.parseInt(br.readLine());
-        t = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        v = Integer.parseInt(st.nextToken());
+        
         board = new boolean[n+1][n+1];
         vis = new boolean[n+1];
-        for(int i =0; i<t; i++){
-            st = new StringTokenizer(br.readLine()); 
+        for(int i =0; i<m; i++){
+            st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            board[a][b] = true;
-            board[b][a] = true;
+            board[a][b] = board[b][a] = true;
         }
-        
+
+        dfs(v);
+        sb.append("\n");
         bfs();
-        System.out.println(ans-1);
+        System.out.println(sb.toString());
+    }
+
+    static void dfs(int from){
+        vis[from] = true;
+        sb.append(from + " ");
+
+        for(int i =1; i<n+1; i++){
+            if(board[from][i] && !vis[i]){
+                dfs(i);
+            }
+        }
     }
 
     static void bfs(){
+        vis = new boolean[n+1];
         Queue<Integer> q = new LinkedList<>();
-        q.add(1);
-        vis[1] = true;
-
+        q.add(v);
+        vis[v] = true;
         while(!q.isEmpty()){
-            ans += 1;
             int now = q.poll();
-            for(int i = 1; i<n+1; i++){
-                if(board[now][i] == true && !vis[i]){
+            sb.append(now + " ");
+            for(int i =1; i<n+1; i++){
+                if(board[now][i] && !vis[i]){
                     vis[i] = true;
                     q.add(i);
                 }
             }
-
         }
     }
 
+    
    
 
 
