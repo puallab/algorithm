d5f9287a4d55deb4b37f68d927ee6109a467cac4
pdh5056 <pdh5056@gmail.com>
2022-08-18 17:02:41 +0900

    ..

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -2,9 +2,8 @@ import java.io.*;
 import java.util.*;
 
 public class Main{
-    static int n, m;
-    static String[] board;
-    static int[][] dist;
+    static int r, c, n;
+    static int[][] board;
     static int[] dy = {0, 0, 1, -1};
     static int[] dx = {1, -1, 0, 0};
 
@@ -19,46 +18,83 @@ public class Main{
             this(0, 0);
         }
     }
+
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
+        r = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
         n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        board = new String[n];
-        dist = new int[n][m];
-        for(int i =0; i<n; i++){
-            board[i] =br.readLine();
+
+        board = new int[r][c];
+
+        for(int i =0; i<r ;i++){
+           String k = br.readLine();
+           for(int j =0; j<c; j++){
+                if(k.charAt(j) !='O'){
+                    board[i][j] = -1;
+                } 
+           }
         }
-       
-        bfs();
+
+        simulate();
+        printAns();
     }
 
-    static void bfs(){
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(0,0));
-        dist[0][0] =1;
-        while(!q.isEmpty()){
-            Pair now = q.poll();
-            for(int i =0; i<4; i++){
-                int y = now.y+ dy[i];
-                int x = now.x+ dx[i];
-                if(y == n-1 && x == m-1){
-                    System.out.println(dist[now.y][now.x]+1);
-                    return;
-                }
-                if(y < 0 || n <= y || x<0 || m <=x || board[y].charAt(x) =='0' ||dist[y][x] != 0 ){
-                    continue;
-                }
+    static void simulate(){
+        int t = 1;
+        while(++t <= n){
+            if(t%2 == 1 ){
+                bomb(t-3);
+            }else{
+                install(t);
+            }
+            
+        }
+    }
 
-                dist[y][x] = dist[now.y][now.x] +1;
-                q.add(new Pair(y, x));
+    static void bomb(int time){
+        for(int i =0; i<r; i++){
+            for(int j=0; j<c; j++){
+                if(board[i][j] == time){
+                    board[i][j] = -1;
+                    for(int k=0; k<4; k++){
+                        int y = i+dy[k], x= j+dx[k];
+                        if(y <0 || y>=r || x<0|| x>=c || board[y][x] == time) continue;
+                        board[y][x] = -1;
+                    }
+                }
             }
         }
     }
 
+    static void install(int time){
+        for(int i =0; i<r; i++){
+            for(int j=0; j<c; j++){
+                if(board[i][j] == -1){
+                    board[i][j]= time;
+                }
+            }
+        }
+    }
 
-   
+    static void printAns(){
+        StringBuilder sb = new StringBuilder();
+
+        for(int i =0; i<r; i++){
+            for(int j=0; j<c; j++){
+                if(board[i][j] == -1){
+                    sb.append(".");
+                }else{
+                    sb.append("O");
+                }
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+
 
 }
 
