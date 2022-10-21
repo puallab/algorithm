0c4ae5ffc356dd425ad39416d483d029c2d68372
paul <39356947+puallab@users.noreply.github.com>
2022-10-21 17:57:02 +0900

    [Gold IV] Title: 가르침, Time: 116 ms, Memory: 11896 KB -BaekjoonHub

---
+++ 백준/Gold/1062. 가르침/가르침.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main{
+    static int n, k, alpha, ans, antic =  1<<'a'-'a' | 1 <<'t'-'a' | 1<< 'i'-'a' | 1 <<'c'-'a' | 1<<'n'-'a';
+    static int[] nbits;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        nbits = new int[n];
+        for(int i =0; i<n; i++){
+            String str= br.readLine();
+            for(char c : str.substring(4, str.length()-4).toCharArray()){
+                if(antic == (antic| (1 << c-'a'))) continue;
+                alpha |= 1 << c-'a';
+                nbits[i] |= 1 << c-'a';
+            }
+        }
+
+        dfs(antic, 0, 5);
+        System.out.println(ans);
+    }
+
+    static void dfs(int bits, int idx, int picked){
+        if(picked == k){
+            ans = Math.max(ans, checkCanRead(bits));
+            return;
+        }
+
+        for(int i = idx; i < 26; i++){
+            if( (bits | (1 << i)) == bits) continue;
+            dfs(bits| (1<<i) , i, picked+1);
+        }
+    }
+
+    static int checkCanRead(int bits){
+        int val = 0;
+        for(int i =0; i< n; i++){
+            if( bits == (bits | nbits[i]))  val++;
+        }
+
+        return val;
+    }
+}
+
