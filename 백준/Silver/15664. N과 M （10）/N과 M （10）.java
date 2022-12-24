bcc1f3355d3023cf861d00fb17cbdd545ba62970
paul <39356947+puallab@users.noreply.github.com>
2022-12-24 11:06:20 +0900

    [Silver II] Title: N과 M (10), Time: 76 ms, Memory: 11572 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception {
+        input();
+        pro();
+    }
+
+    static void input() throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        st= new StringTokenizer(br.readLine());
+        for(int i =0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+    }
+
+    static void pro(){
+        dfs(0,0,"");
+        System.out.println(sb);
+    }
+
+    static void dfs(int idx, int picked , String s){
+        if(picked == m){
+            sb.append(s.trim() + "\n");
+            return;
+        }
+
+        int prev = -1;
+        for(int i =idx; i<n; i++){
+            if(prev == arr[i]) continue;
+            prev = arr[i];
+            dfs(i+1, picked+1, s + " " + arr[i]);
+        }
+    }
+}
