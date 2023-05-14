9d2fc2e284515f42595917d80d3f43165fb99705
paul <39356947+puallab@users.noreply.github.com>
2023-05-14 16:01:58 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.12 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -3,26 +3,47 @@ import java.io.*;
 class Solution {
     public int solution(int n, int k) {
         int answer = 0;
-        String[] arr = Integer.toString(n, k).split("0");
+        String[] arr = change(n,k).split("0");
         
         for(String s : arr){
-            if(s.length() > 0){
-                //System.out.println(s);
-                long value = Long.parseLong(s);    
-                if(value < 2) continue;
-                if(!isPrime(value)) continue;
-                //System.out.println(value);
-                answer++;
-            }
+            if(s.length() == 0 || s == "") continue;
+            long v = Long.parseLong(s);
+            if(!isPrime(v)) continue;
+            answer++;
         }
         
         return answer;
     }
     
-    public static boolean isPrime(long value){
-        for(long i=2; i*i <= value; i++){
-            if(value % i ==0) return false;
+    public static String change(int n, int k){
+        StringBuilder sb = new StringBuilder();
+        
+        while(n >= k){
+            sb.append(n%k);
+            n /= k;
         }
+        sb.append(n);
+        
+        return sb.reverse().toString();
+    }
+    
+    public static boolean isPrime(long value){
+//         if(value < 2) return false;
+        
+//         for(int i = 2; i*i <= value; i++){
+//             if(value % i == 0) return false;
+//         }
+        
+//         return true;
+        if(value == 0 || value == 1) { return false; }
+        for(long i=2; i*i <= value; i++) {
+            if(value % i == 0) {
+                return false;
+            }
+        }
+
         return true;
     }
+    
+    
 }
