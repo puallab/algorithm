8ee05f1e03d696a7f8a5d0501b29c69e97adabcc
paul <39356947+puallab@users.noreply.github.com>
2023-05-21 18:44:38 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 36.42 ms, Memory: 216 MB -BaekjoonHub

--- 프로그래머스/lv3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ 프로그래머스/lv3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -1,31 +1,36 @@
 class Solution {
     public int solution(int[][] board, int[][] skill) {
-        int n = board.length;
-        int m = board[0].length;
-        int[][] d = new int[1003][1003];
-        // 변화량 테이블에 스킬 기록
-        for(int i = 0; i < skill.length; i++){
-            int type = skill[i][0], r1 = skill[i][1], c1 = skill[i][2],
-              r2 = skill[i][3], c2 = skill[i][4], degree = skill[i][5];
-            if(type == 1) degree = -degree;
-            d[r1][c1] += degree;
-            d[r1][c2+1] -= degree;
-            d[r2+1][c1] -= degree;
-            d[r2+1][c2+1] += degree;
+        int answer = 0;
+        int[][] sumBoard = new int[board.length +2][board[0].length+2];
+        for(int i =0; i<skill.length; i++){
+            int type = skill[i][0];
+            int r1 = skill[i][1]+1;
+            int c1 = skill[i][2]+1;
+            int r2 = skill[i][3]+1;
+            int c2 = skill[i][4]+1;
+            int degree = skill[i][5];
+            if(type == 1 ) degree = -degree;
+            
+            sumBoard[r1][c1] += degree;
+            sumBoard[r1][c2+1] += -degree;
+            sumBoard[r2+1][c1] += -degree;
+            sumBoard[r2+1][c2+1] += degree;
         }
-        // 누적합 처리
-        for(int i = 1; i < n; i++)
-            for(int j = 0; j < m; j++)
-                d[i][j] += d[i-1][j];
-
-        for(int i = 0; i < n; i++)
-            for(int j = 1; j < m; j++)
-                d[i][j] += d[i][j-1];
-
-        int ans = 0;
-        for(int i = 0; i < n; i++)
-            for(int j = 0; j < m; j++)
-                if(d[i][j]+board[i][j] > 0) ans++;
-        return ans;
+        
+        for(int i=1; i<sumBoard.length; i++){
+            for(int j =1; j<sumBoard[i].length; j++){
+                sumBoard[i][j] += sumBoard[i][j-1] + sumBoard[i-1][j] - sumBoard[i-1][j-1]; 
+            }
+        }
+        
+        for(int i=1; i<sumBoard.length-1; i++){
+            for(int j =1; j<sumBoard[i].length-1; j++){
+                if(sumBoard[i][j] + board[i-1][j-1] > 0){
+                    answer++;
+                }
+            }
+        }
+        
+        return answer;
     }
 }
