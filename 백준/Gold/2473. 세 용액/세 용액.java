c924db22cd6dbf60ae9c45aeab17bf859152d41c
paul <39356947+puallab@users.noreply.github.com>
2023-10-06 16:48:47 +0900

    [Gold III] Title: 세 용액, Time: 976 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.java
+++ 백준/Gold/2473. 세 용액/세 용액.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class Main {
 	static int n;
-	static int[] arr, ans = new int[3];
+	static int[] arr;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
@@ -15,45 +15,47 @@ public class Main {
 		}
 
 		Arrays.sort(arr);
-		solve();
-		Arrays.sort(ans);
-		System.out.println(ans[0] + " " + ans[1] + " " + ans[2]);
-	}
-
-	static void solve(){
 		long minVal = Long.MAX_VALUE;
-		
+		int[] ans = new int[3];
 		for(int i =0; i<n; i++){
-			int fixed = arr[i];
-			int left = i;
-			int right = n-1;
-			while(left< right){
-				if(left == i){
-					left++;
-					continue;
+			for(int j =i+1; j<n; j++){
+				long val = (long)arr[i]+arr[j];
+				int findIdx = find(i, j, val);
+				if(findIdx == -1) continue;
+				if(minVal > Math.abs(val+arr[findIdx])){
+					ans[0] = arr[i];
+					ans[1] = arr[j];
+					ans[2] = arr[findIdx];
+					minVal = Math.abs(val+arr[findIdx]);
 				}
-				if(right == i){
-					right--;
-					continue;
-				}
-
-				long val = (long)fixed + (long)arr[left] + arr[right];
-				if(Math.abs(val) < minVal){
-					minVal = Math.abs(val);
-					ans[0] = arr[left];
-					ans[1] = arr[right];
-					ans[2] = fixed;
-				}
-				if(val > 0){
-					right--;
-				}else if(val <0){
-					left++;
-				}else{
-					return;
-				}
-				
 			}
+		}
+		Arrays.sort(ans);
+		for(int i : ans){
+			System.out.print(i + " ");
 		}
 	}
 
+	static int find(int idx1, int idx2, long val){
+		int left  =0;
+		int right = n-1;
+		long target = Long.MAX_VALUE;
+		int idx = -1;
+		
+		while(left <= right){
+			int mid = (left+right)/2;
+			if(val+arr[mid] < 0){
+				left = mid +1;
+			}else if(val+arr[mid] >= 0){
+				right = mid -1;
+			}
+			if(Math.abs(val+arr[mid]) < Math.abs(target)){
+				if(mid == idx1 || mid == idx2) continue;
+				target = Math.abs(val+arr[mid]);
+				idx = mid;
+			}
+		}
+
+		return idx;
+	}
 }
